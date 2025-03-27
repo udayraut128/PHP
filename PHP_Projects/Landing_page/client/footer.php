1eca9c13d1f76f98d6837595791e26d5bb738790
Uday Raut <rautuday75@gmail.com>
2025-03-27 13:59:04 +0530

    php pro

---
+++ PHP_Projects/Landing_page/client/footer.php
@@ -0,0 +1,6 @@
+<div class="flex gap-4">
+    <div>logo</div>
+    <div>link</div>
+    <div>cpages</div>
+    <div>contect</div>
+</div>
---
+++ PHP_Projects/Landing_page/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <?php include('./client/commonfile.php') ?>
+</head>
+<body>
+    <div class="100vh"><?php include('client/header.php') ?></div>
+    <div><?php include('client/main.php') ?></div>
+    <div><?php include('client/footer.php') ?></div>
+    
+     
+    
+</body>
+</html>
